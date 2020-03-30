afd6acaf0ebb99685c6eff30f8a94f8666e9724b
Simeon Franklin <simeonf@gmail.com>
2020-03-30 08:29:42 -0700

    Enable login url

--- nas/nas/urls.py
+++ nas/nas/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
     path('', include('video.urls')),
 ]
